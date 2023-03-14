5beac9ad713d41a2a640210e82edaf1e0f278921
keirabea <keirabeamoore@gmail.com>
2023-03-14 12:32:40 +0000

    movement2_sway
    tried a different method of moving the character, added a rigid body and a collider for the floor. character sways side to side

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    PlayerControl controls;
+    float direction = 0;
+
+    public float speed = 400;
+
+    public Rigidbody2D playerRB;
+
+    //initialising controls
+
+    private void Awake()
+    {
+        controls = new PlayerControl();
+        controls.Enable();
+        //checking for 1d axis with the action mat
+        controls.room.move.performed += ctx =>
+        {
+            direction = ctx.ReadValue<float>();
+        };
+    }
+
+
+    // Update is called once per frame
+    // update is to control the speed of the player and edit their x and y position
+    void Update()
+    {
+        playerRB.velocity = new Vector2(direction * speed * Time.deltaTime, playerRB.velocity.y);
+        
+    }
+}
